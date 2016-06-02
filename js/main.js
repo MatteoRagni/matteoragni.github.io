3f4a562372ea62838875b73fda223dbda0194ac4
MatteoRgni <matteo.ragni.it@gmail.com>
2016-06-03 00:06:46 +0100

    coming soon

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//  __  __      _   _             ___                _   ___
+// |  \/  |__ _| |_| |_ ___ ___  | _ \__ _ __ _ _ _ (_) | _ \___ ____  _ _ __  ___
+// | |\/| / _` |  _|  _/ -_) _ \ |   / _` / _` | ' \| | |   / -_|_-< || | '  \/ -_)
+// |_|  |_\__,_|\__|\__\___\___/ |_|_\__,_\__, |_||_|_| |_|_\___/__/\_,_|_|_|_\___|
+//                                        |___/
+
+$(document).ready(function(e) {
+    var property = {
+      dotColor: "#aaa",
+      lineColor: "#999",
+      maxSpeedY: 0.8,
+      maxSpeedX: 0.8,
+      particleRadius: 5,
+      lineWidth: 0.5,
+      parallaxMultiplier: 50,
+      density: 15000
+    }
+    particleground(document.getElementById("b1"), property);
+    particleground(document.getElementById("b2"), property);
+    //particleground(document.getElementById("b3"), property);
+
+    new WOW().init();
+});
